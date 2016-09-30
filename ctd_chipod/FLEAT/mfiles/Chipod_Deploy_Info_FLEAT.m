2b1a2d1ff36b6de93d47f3ff6ea543650e4ed629
andypicke <andypicke@gmail.com>
2016-09-30 12:36:47 -0600

    modify templates

---
+++ ctd_chipod/FLEAT/mfiles/Chipod_Deploy_Info_FLEAT.m
@@ -0,0 +1,71 @@
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%
+% Chipod_Deploy_Info_FLEAT.m
+%
+% Deployment info for CTD chipods. For loading during chipod
+% processing.
+%
+% Notes:
+%
+% (1) The sign of az_correction is not always consistent with the
+% chipod deployment direction (some units are wired oppositely);
+% the correct sign needs to be determined from aligning the chipod and 
+% CTD (AlignChipodCTD.m in the processing script).
+%
+%
+%---------------
+% 09/30/16 - A.Pickering - andypicke@gmail.com
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%%
+
+ChiInfo=struct();
+ChiInfo.Project='FLEAT';           % Project name
+ChiInfo.SNs={'SN2014','SN2002','SN1013'}; % List of chipod SNs
+ChiInfo.CastString='fleat16';       % Identifying string in CTD cast files
+
+%%~~~~~~~~~~~~~~~~~~~
+% SN 1013 
+clear S SN
+SN='1013';
+S.loggerSN=SN;       % logger serial number
+S.pcaseSN='Ti44-11'; % pressure case SN
+S.sensorSN='14-34D'; % sensor SN
+S.InstDir='up';      % mounting direction (of sensor) on CTD
+S.InstType='mini';   % Instrument type ('mini' or 'big')
+S.isbig=0;           % 1 for 'big' chipods
+S.az_correction=-1;  % See note above
+S.suffix='mlg';      % suffix for chipod raw data filenames
+S.cal.coef.T1P=0.097;% TP calibration coeff (time constant)
+ChiInfo.(['SN' SN])=S;clear S
+
+%
+clear S SN
+SN='2002';
+S.loggerSN=SN;       % logger serial number
+S.pcaseSN='Ti44-11'; % pressure case SN
+S.sensorSN='14-34D'; % sensor SN
+S.InstDir='down';      % mounting direction (of sensor) on CTD
+S.InstType='mini';   % Instrument type ('mini' or 'big')
+S.isbig=0;           % 1 for 'big' chipods
+S.az_correction=1;  % See note above
+S.suffix='mlg';      % suffix for chipod raw data filenames
+S.cal.coef.T1P=0.097;% TP calibration coeff (time constant)
+ChiInfo.(['SN' SN])=S;clear S
+
+% 
+clear S SN
+SN='2014';
+S.loggerSN=SN;       % logger serial number
+S.pcaseSN='Ti44-11'; % pressure case SN
+S.sensorSN='14-34D'; % sensor SN
+S.InstDir='up';      % mounting direction (of sensor) on CTD
+S.InstType='mini';   % Instrument type ('mini' or 'big')
+S.isbig=0;           % 1 for 'big' chipods
+S.az_correction=1;  % See note above
+S.suffix='mlg';      % suffix for chipod raw data filenames
+S.cal.coef.T1P=0.097;% TP calibration coeff (time constant)
+ChiInfo.(['SN' SN])=S;clear S
+
+% ***
+ChiInfo.MakeInfo='Chipod_Deploy_Info_FLEAT.m';
+%%
--- ctd_chipod/FLEAT/mfiles/Load_chipod_paths_FLEAT.m
+++ ctd_chipod/FLEAT/mfiles/Load_chipod_paths_FLEAT.m
@@ -1,20 +1,21 @@
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 %
-% Load_chipod_paths_Template.m
+% Load_chipod_paths_FLEAT.m
 %
 % See also process_chipod_script_template.m
 %
 %------------------
-% July 7, 2015 - A. Pickering - apickering@coas.oregonstate.edu
-% 06/12/1 - AP - New format, now all paths defined relative to BaseDir
+% 09/30/16 - A. Pickering - apickering@coas.oregonstate.edu
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 %%
 
 %*** Replace 'IO9' with project name
-BaseDir='/Users/Andy/Cruises_Research/ChiPod/IO9/'
+%BaseDir='/Users/Andy/Cruises_Research/ChiPod/FLEAT/'
+BaseDir=fullfile('/Volumes/SP PHD U3/NonBackup/FLEAT/')
 
 % Folder with raw CTD data (.hex and .XMLCON files)
-CTD_data_dir=fullfile(BaseDir,'Data','raw','CTD')
+%CTD_data_dir=fullfile(BaseDir,'Data','raw','CTD')
+CTD_data_dir=fullfile('/Volumes/SP PHD U3/NonBackup/FLEAT/Data/raw/ctd')
 
 % Base directory for all processed CTD output
 CTD_out_dir_root=fullfile(BaseDir,'Data','proc','CTD')
@@ -29,10 +30,11 @@ CTD_out_dir_bin=fullfile(CTD_out_dir_root,'binned')
 CTD_out_dir_figs=fullfile(CTD_out_dir_root,'figures')
 
 % folder for raw chi pod data
-chi_data_path=fullfile(BaseDir,'Data','raw','Chipod')
+%chi_data_path=fullfile(BaseDir,'Data','raw','Chipod')
+chi_data_path=fullfile('/Volumes/SP PHD U3/NonBackup/FLEAT/Data/raw/chipod')
 
 % folder for processed chipod output
-chi_proc_path=fullfile(BaseDir,'Data','proc','Chipod')
+chi_proc_path=fullfile('/Volumes/SP PHD U3/NonBackup/FLEAT/','Data','proc','Chipod')
 
 ChkMkDir(CTD_out_dir_root)
 ChkMkDir(CTD_out_dir_bin)
