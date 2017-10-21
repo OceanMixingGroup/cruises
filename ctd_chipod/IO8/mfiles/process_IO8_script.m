f120d4858be9d3fb3b47c4fe1800aae9ac60d7da
andypicke <andypicke@gmail.com>
2017-10-21 10:08:15 -0600

    change Project to the_project
    project is a Matlab function, so change to the_project to avoid issues.

--- ctd_chipod/IO8/mfiles/process_IO8_script.m
+++ ctd_chipod/IO8/mfiles/process_IO8_script.m
@@ -14,15 +14,21 @@ clear ; close all
 % *** Should only need to modify below section, 'Load_chipod_paths_', and 
 % Chipod_Deploy_Info_... files
 %~~~~~~~~~~~~~~~~~ 
-Project='IO8'
+the_project='IO8'
 
 % path to 'mixingsoftware' 
-mixpath = '/Users/Andy/Cruises_Research/mixingsoftware/';
+mixpath = '/Users/Andy/Cruises_Research/mixingsoftwae/';
 
 %~~~~~~~~~~~~~~~~~
 
-eval(['Load_chipod_paths_' Project])
-eval(['Chipod_Deploy_Info_' Project])
+eval(['Load_chipod_paths_' the_project])
+eval(['Chipod_Deploy_Info_' the_project])
+
+
+status_out = check_data_struct_ctd_chipod(the_project,mixpath);
+if status_out~=1
+    error('error: missing or incorrect folder structure')
+end
 
 
 % Add paths we will need
@@ -35,15 +41,15 @@ addpath(fullfile(mixpath,'adcp'))      ;% need for mergefields_jn.m in load_chip
 
 %% Make plots of the raw chipod files
 
-PlotChipodDataRaw_General(Project,mixpath)
+PlotChipodDataRaw_General(the_project,mixpath)
 
 %% Make 'casts'; get chipod data for each CTD profile and save separately
 
-MakeCasts_CTDchipod_function(Project,mixpath)
+MakeCasts_CTDchipod_function(the_project,mixpath)
 
 %% Plot chipod TP for each cast/instrument
 
-Plot_TP_profiles_EachCast_CTDchipod(Project)
+Plot_TP_profiles_EachCast_CTDchipod(the_project)
 
 %%
 % replace w/ generic***
@@ -51,7 +57,7 @@ Plot_TP_profiles_EachCast_CTDchipod(Project)
 
 %% Make a summary of chipod data/processing
 
-SummarizeChiProc(Project)
+SummarizeChiProc(the_project)
 
 %% Optional; inspect and mark bad profiles so they don't get processed
 
@@ -64,19 +70,19 @@ SummarizeChiProc(Project)
 
 % * Uses default Params for now, need to add Params option to function
 % input
-do_chi_calc_ctd_chipod(Project,mixpath)
+do_chi_calc_ctd_chipod(the_project,mixpath)
 
 %% Combine all profiles into a single structure 'XC'
 
-XC = make_combined_chi_struct(Project,mixpath)
+XC = make_combined_chi_struct(the_project,mixpath)
 
 %% Make some standard plots (ie depth-lat transects) from 'XC'
 
-plot_XC_summaries(Project)
+plot_XC_summaries(the_project)
 
 %% Write automated latex notes file w/ some standard info and plots
 
-Write_Latex_Notes(Project)
+Write_Latex_Notes(the_project)
 
 
 %%
